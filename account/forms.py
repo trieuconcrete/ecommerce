976d868aae2d7d11da8c7a9df0f79e69ece5206c
trieuconcrete <trieunb@concrete-corp.com>
2025-03-03 11:02:26 +0700

    update UI/UX

--- account/forms.py
+++ account/forms.py
@@ -42,3 +42,22 @@ class UpdateUserForm(forms.ModelForm):
 
         fields = ['username', 'email']
         exclude = ['password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+        # Make email field as required
+        self.fields['email'].required = True
+
+    # Email validation
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('This email is invalid')
+        
+        if len(email) >= 350:
+            raise forms.ValidationError('Your email is too long')
+        
+        return email
+
